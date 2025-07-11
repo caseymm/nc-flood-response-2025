d2ede3547504bcb4f38307efa5b9ce7b704778ce
caseymm <caseymm@gmail.com>
2025-07-11 17:18:46 -0400

    add base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/nc-flood-response-2025/", // REQUIRED for GitHub Pages unless you're using a custom domain
+});
